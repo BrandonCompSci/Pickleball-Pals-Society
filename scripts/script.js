3fd3bf941a146ead236fceda7b1ee275fe58268b
Brandon Gautier <brandoncompsci@protonmail.com>
2024-03-30 22:03:43 -0600

    Edit svg

--- scripts/script.js
+++ scripts/script.js
@@ -22,3 +22,34 @@ $(document).ready(function() {
         indicator: 'plusminus',
     });
 })
+
+// Implement a Scroll To Top Button
+    // Code sourced from: 
+    // w3schools.com. 2023. How To Create a Scroll To Top Button. [online] Available at: 
+    // <https://www.w3schools.com/howto/howto_js_scroll_to_top.asp> [Accessed 25 April 2023].
+    function scrollFunction() {
+        // Get the button:
+        let myButton = document.getElementById("go-top");
+    
+        // Display button when document is scrolled greater than 20 pixels
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            myButton.style.display = "flex";
+        } else {
+            myButton.style.display = "none";
+        }
+    }
+
+    // Event listener for back-to-top button
+    const backToTop = document.getElementById("go-top");
+    backToTop.addEventListener("click", () => {
+        topFunction();
+    });
+
+    // When the user scrolls down 20px from the top of the document, show the button
+    window.onscroll = function() {scrollFunction()};  
+    
+    // When the user clicks on the button, scroll to the top of the document
+    function topFunction() {
+        document.body.scrollTop = 0; // For Safari
+        document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    }
